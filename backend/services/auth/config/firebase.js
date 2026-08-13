271232bc3a5c2771755e043cc51c825aa92d56ae
Harsh-Kumar14 <harsh.rnhs8055@gmail.com>
2026-08-13 10:30:58 +0530

    update firebase.js

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,7 +1,17 @@
+// import { initializeApp, cert } from "firebase-admin";
+
+// import serviceAccount from "../serviceAccountKey.json" with {type: "json"};
+
+// export const app=initializeApp({
+//     credential:cert(serviceAccount)
+// })
+
 import { initializeApp, cert } from "firebase-admin";
 
-import serviceAccount from "../serviceAccountKey.json" with {type: "json"};
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString("utf8")
+);
 
-export const app=initializeApp({
-    credential:cert(serviceAccount)
-})
+export const app = initializeApp({
+  credential: cert(serviceAccount),
+});
